615f4b95ac427a3fc5bcc207802258f459ce62ca
Pete Thorne <pete.thorne@learningpool.com>
2024-05-06 14:09:11 +0100

    Fixed controller mouse rotation

--- player.gd
+++ player.gd
@@ -1,37 +1,44 @@
 extends CharacterBody3D
 
-class_name Player
-
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.3  # Adjust this value to change mouse sensitivity
 
+@onready var pivot: Node3D = $CamOrigin
+@onready var camera: Camera3D = $CamOrigin/Camera3D
 
-func _physics_process(delta: float) -> void:
-	Debug.print_value("PlayerState", "Idle")
-	Debug.print_value("Test", "Idle")
-	# Add the gravity.
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
+		pivot.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
+		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta):
 	if not is_on_floor():
-		Debug.print_value("PlayerState", "Not on floor")
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		Debug.print_value("Jumping", "True")
 		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	Debug.print_value("Direction", var_to_str(direction))
-	
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
-		Debug.print_value("Moving", "True")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		Debug.print_value("Moving", "False")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
